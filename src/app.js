d8cc50afcec6ef835c0cf34af0f2bafc1878c098
Henrique <henriqueluiz266@gmail.com>
2023-07-01 17:49:06 -0300

    post /messages

--- src/app.js
+++ src/app.js
@@ -80,6 +80,53 @@ app.post("/participants", async (req, res) => {
   }
 });
 
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+  console.log(user);
+
+  const schemaUser = joi.string().required();
+
+  const validation1 = schemaUser.validate(user);
+
+  const schemaMessage = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().allow("message", "private_message"),
+  });
+
+  const validation2 = schemaMessage.validate(req.body, { abortEarly: false });
+
+  const from = await db.collection("participants").findOne({ name: user });
+
+  if (validation1.error || validation2.error || !from) {
+    return res.sendStatus(422);
+  }
+
+  try {
+    await db.collection("messages").insertOne({
+      from: user,
+      to: to,
+      text: text,
+      type: type,
+      time: dayjs().format("HH:mm:ss"),
+    });
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 // Ligar a aplicação do servidor para ouvir requisições
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rondando na porta ${PORT}.`));
